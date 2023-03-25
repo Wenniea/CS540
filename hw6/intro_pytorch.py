cb83477c5b7e8e3892689b6f042ab8bde63eaa8d
Wenniea <wenjingj1231@gmail.com>
2023-03-25 17:25:42 -0500

    reformat and edited evaluate_model

--- hw6/intro_pytorch.py
+++ hw6/intro_pytorch.py
@@ -8,18 +8,7 @@ from torchvision import datasets, transforms
 # Feel free to import other packages, if needed.
 # As long as they are supported by CSL machines.
 
-
 def get_data_loader(training=True):
-    """
-    TODO: implement this function.
-
-    INPUT: 
-        An optional boolean argument (default value is True for training dataset)
-
-    RETURNS:
-        Dataloader for the training set (if training = True) or the test set (if training = False)
-    """
-
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
@@ -39,15 +28,6 @@ def get_data_loader(training=True):
 
 
 def build_model():
-    """
-    TODO: implement this function.
-
-    INPUT: 
-        None
-
-    RETURNS:
-        An untrained neural network model
-    """
     model = nn.Sequential(
         nn.Flatten(),
         nn.Linear(28 * 28, 128),
@@ -60,19 +40,6 @@ def build_model():
 
 
 def train_model(model, train_loader, criterion, T):
-    """
-    TODO: implement this function.
-
-    INPUT: 
-        model - the model produced by the previous function
-        train_loader  - the train DataLoader produced by the first function
-        criterion   - cross-entropy 
-        T - number of epochs for training
-
-    RETURNS:
-        None
-    """
-
     opt = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     for epoch in range(T):
         running_loss = 0
@@ -81,6 +48,7 @@ def train_model(model, train_loader, criterion, T):
         total = 0
         for i, data in enumerate(train_loader, 0):
             inputs, labels = data
+            batch_size = inputs.size(0)
 
             opt.zero_grad()
 
@@ -88,30 +56,17 @@ def train_model(model, train_loader, criterion, T):
             loss = criterion(outputs, labels)
             loss.backward()
             opt.step()
-
-            running_loss += loss.item() * 64
+            running_loss += loss.item() * batch_size
 
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-        print(f'Train Epoch: {epoch}   Accuracy: {correct}/{total}({100 * correct / total:.2f}%) '
+        print(f'Train Epoch: {epoch} Accuracy: {correct}/{total}({100 * correct / total:.2f}%) '
               f'Loss: {running_loss / 60000:.3f}')
 
 
 def evaluate_model(model, test_loader, criterion, show_loss=True):
-    """
-    TODO: implement this function.
-
-    INPUT: 
-        model - the the trained model produced by the previous function
-        test_loader    - the test DataLoader
-        criterion   - cropy-entropy 
-
-    RETURNS:
-        None
-    """
-
     model.eval()
     with torch.no_grad():
         total = 0
@@ -119,37 +74,26 @@ def evaluate_model(model, test_loader, criterion, show_loss=True):
         running_loss = 0
         for i, data in enumerate(test_loader, 0):
             inputs, labels = data
+            batch_size = inputs.size(0)
 
             outputs = model(inputs)
             loss = criterion(outputs, labels)
 
-            running_loss += loss.item() * 64
+            running_loss += loss.item() * batch_size
 
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-        acc = correct / total
+        accuracy = correct / total
         if show_loss:
-            print(f'Loss: {running_loss / 60000:.4f}')
-            print(f'Accuracy: {100 * acc:.2f}%')
+            print(f'Average loss: {running_loss / 60000:.4f}')
+            print(f'Accuracy: {100 * accuracy:.2f}%')
         else:
-            print(f'Accuracy: {100 * acc:.2f}%')
+            print(f'Accuracy: {100 * accuracy:.2f}%')
 
 
 def predict_label(model, test_images, index):
-    """
-    TODO: implement this function.
-
-    INPUT: 
-        model - the trained model
-        test_images   -  a tensor. test image set of shape Nx1x28x28
-        index   -  specific index  i of the image to be tested: 0 <= i <= N - 1
-
-
-    RETURNS:
-        None
-    """
     class_names = ['T - shirt / top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt'
         , 'Sneaker', 'Bag', 'Ankle Boot']
 
@@ -170,7 +114,6 @@ if __name__ == '__main__':
     Feel free to write your own test code here to exaime the correctness of your functions. 
     Note that this part will not be graded.
     '''
-    torch.manual_seed(0)
     criterion = nn.CrossEntropyLoss()
     train_loader = get_data_loader()
     print(type(train_loader))
@@ -179,6 +122,6 @@ if __name__ == '__main__':
     model = build_model()
     print(model)
     train_model(model, train_loader, criterion, 5)
-    evaluate_model(model, test_loader, criterion, show_loss=False)
+    evaluate_model(model, test_loader, criterion, show_loss=True)
     test_images, test_labels = next(iter(test_loader))
     predict_label(model, test_images, 1)
